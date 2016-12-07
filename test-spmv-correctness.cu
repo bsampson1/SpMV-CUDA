f469d7a79600e2695e3a1b405ebf66be33158831
mac34 <mechealgreene@icloud.com>
2016-12-07 08:52:48 -0500

    cpu to gpu test

--- test-spmv-correctness.cu
+++ test-spmv-correctness.cu
@@ -69,8 +69,14 @@ printf("Output vector y: "); printArray(y_cpu, N);
 //Calculate the result with the normal CPU implementation and compare the results with the GPU(Test)
 //GPU first & then CPU(Didn't add GPU code to compare to the CPU code yet)
 
+    //int row = threadIdx.x;
+    //y[row] = 0;
+    //int j;
+        for (j = IA[row]; j < IA[row+1]; ++j)
+        {
+            y[row] += A[j]*x[JA[j]];
+        }
     int compare = 0;
-    float sum;
         for (i = 0; i < A.M; ++i)
         {
             sum = 0;
@@ -79,6 +85,13 @@ printf("Output vector y: "); printArray(y_cpu, N);
                     sum += A.A[j]*x[A.JA[j]];
                 }
                     y[i] = sum;
+                    if (y[row]==y[i])
+                    {
+                        compare++;
+                    }else{
+                        compare+=0;
+                    }
+
         }
     if(compare == M*N){
         printf("Success!\n");

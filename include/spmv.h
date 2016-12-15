7d4de49ca1b7ba2ce6fc01da445779e04924fd08
bsampson1 <bsampson@udel.edu>
2016-12-15 01:02:35 -0500

    Updated comments in spmv.h

--- include/spmv.h
+++ include/spmv.h
@@ -4,25 +4,22 @@
 #define BLOCK_SIZE 1024
 
 void printArray(const float* arr, const int l);
-/* Used to print A array from CSR formatted sparse matrix
+/* Prints float array of length l
  */
 
 void printArray(const int* arr, const int l);
-/* Used to print IA & JA array from CSR formatted sparse matrix
+/* Prints int array of length l
  */
 
 void cpuSpMV(float *y, float *A, int *IA, int *JA, const int M, const float *x);
-/* For testing and correctness purposes.
- * CPU implementation of matrix-vector multiplication for sparse
+/* CPU implementation of matrix-vector multiplication for SpMV multiplcation
 */
 
 bool areEqualRMSE(const float *a, const float *b, const int N);
 /* Checks whether the rmse between float array a and b are below
  * some given rmse threshold. If it is, we can consider a and b 
  * equal and if not, we can consider they not equal. Internal
- * RMSE_THRESHOLD = 1e-5
- *      a, b - input float vectors
- *      N - length of vector
+ * RMSE_THRESHOLD = 1e-3
  */
 
 __global__
@@ -30,46 +27,29 @@ void spmvChocolate(float *y, const float *A, const int *IA, const int *JA, const
 /* Sparse matrix-vector multiplication for GPU
  * One thread computes one element of output vector y
  * but after loading to shared memory first
- *      y - output vector (Mx1)
- *      A - sparse matrix (MxN)
- *      x - input vector (Nx1)
- *      M - number of rows of A
- *      NNZ - number of non-zero elements of A
  */
 
 __global__
 void spmvVanilla(float *y, const float *A, const int *IA, const int *JA, const float *x);
 /* Sparse matrix-vector multiplication for GPU
- * One thread computes one element of output vector
- *      y - output vector (M x 1)
- *      A - sparse matrix (M x N)
- *      x - input vector  (N x 1)
+ * One thread computes one element of output vector using global memory
  */
 
+// WARNING: DO NOT USE THIS FUNCTION FOR LARGE MATRICES
 void printSpMatrix(const float* A, const int* IA, const int* JA, const int M, const int N);
-/* Takes in a sparse matrix in CSR format and prints the entire matrix using printf
- * WARNING: DO NOT USE THIS FUNCTION FOR LARGE MATRICES
- *      S - input sparse matrix
+/* Prints a CSR-formatted sparse matrix
  */
 
 // WARNING: Generating sparse matrices with size N > 2^15 is prohibitively expensive for memory & time! Generally avoid doing this.
 void generateSquareSpMatrix(float **A_p, int **IA_p, int **JA_p, int *NNZ_p, const int N, const double p_diag, const double p_nondiag);
-/* Takes in a MxN size and two probabilities and generates one realization of a sparse matrix
- *      N - number of row (and columns)
- *      p_diag - probability of non-zero element on main diagonal
- *      p_nondiag - probability of non-zero element not on main diagonal
+/* Generates a square sparse matrix of dimension N in CSR format with elements of the diagonal according to probability p_diag and
+ * elements off the diagonal according to probability p_nondiag
  */
 
 void resizeSpMatrixArraysAndCopy(float **A_temp_p, int **JA_temp_p, int *bufferSize_p, double RESIZE_FACTOR);
-/* Called from within generateSquareSpMatrix when we've exceeded the allowed buffer size.
- * The function will resize the temp arrays to length bufferSize * RESIZE_FACTOR provided in
- * the general case that RESIZE_FACTOR > 1. If you provide a RESIZE_FACTOR <= 1, it will be
- * resized by a factor of 1.33 by default. By the end of the function, bufferSize will point
- * to an integer which is the new length of the temporary sparse matrix arrays.
- *      A_temp - array that holds the float data of sparse matrix in CSR format
- *      JA_temp - array that holds the column indicies of sparse matrix in CSR format
- *      bufferSize - lengths of A_temp and JA_temp
- *      RESIZE_FACTOR - a constant determining the length of new temp arrays (should be > 1!)
+/* Called from within generateSquareSpMatrix function when we have run out of room to store new elements
+ * in A & JA. Will create new arrays of length RESIZE_FACTOR times the original length (bufferSize) and 
+ * copy the elements into the new larger array.
  */
 
 float getRandomFloat(const float min, const float max);
@@ -77,25 +57,8 @@ float getRandomFloat(const float min, const float max);
  */
 
 void fillDenseVector(float* v, const int N);
-/* Takes in a pointer, v, to a 1-D vector and a length N and fills it with random values
- * v - input vector pointer
- * N - input vector length
- */
-
-//typedef struct
-//{
-//        int M, N, NNZ;
-//        float* A;
-//        int* IA;
-//        int* JA;
-//} SpMatrix;
-/* Sparse Matrix Struct - CSR Format
- *      M - number of rows
- *      N - number of columns
- *      NNZ - number of nonzero elements
- *      A - pointer to float array containing nonzero elements (length NNZ)
- *      IA - pointer to row pointer array (length M+1)
- *      JA - pointer to column index array (length NNZ)
+/* Takes in a pointer to a vector and fills it with random floats
+ * using getRandomFloat
  */
 
 #endif

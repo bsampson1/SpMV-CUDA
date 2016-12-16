8aef693e22de940baf7f285d63b4f9c82d45ef46
bsampson1 <bsampson@udel.edu>
2016-12-16 12:37:41 -0500

    Added timing test to main.cu to run over a given number of iterations for a SpMV flavor

--- src/main.cu
+++ src/main.cu
@@ -1,107 +1,156 @@
 #include "spmv.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/time.h>
+#include <math.h>
 
 int main()
 {
+        // Do gettimeofday timing for estimating execution time of main
+        struct timeval t1_main, t2_main;
+        double elapsedMain;
+        gettimeofday(&t1_main, NULL);
+
+        printf("Running main using spmvChocolate\n");
+
         // PARAMETERS
         double p_diag = 0.9;
         double p_nondiag = 0.1;
-        int N = (1 << 12);
         float *A_cpu, *A_gpu, *x_cpu, *x_gpu, *y_cpu, *y_gpu, *y_correct;
         int *IA_cpu, *IA_gpu, *JA_cpu, *JA_gpu;
         int NNZ;
- 
+
+        int expNmin = 10;
+        int expNmax = 15;
+        int Nmin = (1 << expNmin);
+        int Nmax = (1 << expNmax);
+        int L = expNmax-expNmin+1; //printf("L = %i\n", L);
+        float *t_arr = (float *)malloc(sizeof(float)*L);
+        int *N_arr = (int *)malloc(sizeof(float)*L);
+        int i; int idx = 0;
+        for (i = 0; i < L; ++i)
+                N_arr[i] = (1 << (i+expNmin));
+
         // seed random number generator
         time_t t; srand((unsigned) time(&t));
 
-        printf("Computing spmv for %ix%i sparse matrix\n", N, N);
+        const int NUM_ITERS = 20;
 
-        // Define cuda events
+        // Define cuda events for GPU timing
+        float milliseconds;
         cudaEvent_t start, stop;
         cudaEventCreate(&start);
         cudaEventCreate(&stop);
-
-        // Create sparse matrix
-        generateSquareSpMatrix(&A_cpu, &IA_cpu, &JA_cpu, &NNZ, N, p_diag, p_nondiag); // allocates!
-
-        // Verify correctness by hand
-        //printf("Sparse Matrix S: \n"); printSpMatrix(S);
-        //printf("A: "); printArray(A_cpu, NNZ);
-        //printf("IA: "); printArray(IA_cpu, N+1);
-        //printf("JA: "); printArray(JA_cpu, NNZ);
-
-        // Generate dense vector x
-        x_cpu = (float *)malloc(sizeof(float)*N);
-        fillDenseVector(x_cpu, N);
-        //printf("Dense vector x: "); printArray(x_cpu, N);
         
-        // Define output vector y
-        y_cpu = (float *)malloc(sizeof(float)*N);
-
-        // Setup memory on the GPU
-        cudaMalloc((void**) &A_gpu, NNZ*sizeof(float));
-        cudaMalloc((void**) &IA_gpu, (N+1)*sizeof(int)); // N = M
-        cudaMalloc((void**) &JA_gpu, NNZ*sizeof(int));
-        cudaMalloc((void**) &x_gpu, N*sizeof(float));
-        cudaMalloc((void**) &y_gpu, N*sizeof(float)); // N = M
-        
-        
-        // Transfer to device
-        cudaMemcpy(A_gpu, A_cpu, NNZ*sizeof(float), cudaMemcpyHostToDevice);
-        cudaMemcpy(IA_gpu, IA_cpu, (N+1)*sizeof(int), cudaMemcpyHostToDevice);
-        cudaMemcpy(JA_gpu, JA_cpu, NNZ*sizeof(int), cudaMemcpyHostToDevice);
-        cudaMemcpy(x_gpu, x_cpu, N*sizeof(float), cudaMemcpyHostToDevice);
-
-        // CUDA kernel parameters
-        int dB, dG;
-        if (N < 1024)
+        int N, iter; double elapsed;
+        for (N = Nmin; N <= Nmax; N=N*2)
         {
-                dB = N;
-                dG = 1;
-        }
-        else
-        {
-                dB = BLOCK_SIZE;
-                dG = N / BLOCK_SIZE;
-        }
+                elapsed = 0;
+                for (iter = 0; iter < NUM_ITERS; ++iter)
+                {
+                        // Create sparse matrix
+                        generateSquareSpMatrix(&A_cpu, &IA_cpu, &JA_cpu, &NNZ, N, p_diag, p_nondiag); // allocates!
 
-        // Start cudaEvent timing
-        cudaEventRecord(start);
+                        // Generate dense vector x
+                        x_cpu = (float *)malloc(sizeof(float)*N);
+                        fillDenseVector(x_cpu, N);
+                        
+                        // Define output vector y and y_correct
+                        y_cpu = (float *)malloc(sizeof(float)*N);
+                        y_correct = (float *)malloc(sizeof(float)*N);
+
+                        // Setup memory on the GPU
+                        cudaMalloc((void**) &A_gpu, NNZ*sizeof(float));
+                        cudaMalloc((void**) &IA_gpu, (N+1)*sizeof(int)); // N = M
+                        cudaMalloc((void**) &JA_gpu, NNZ*sizeof(int));
+                        cudaMalloc((void**) &x_gpu, N*sizeof(float));
+                        cudaMalloc((void**) &y_gpu, N*sizeof(float)); // N = M
         
-        // Vanilla SpMV CUDA Kernel
-        spmvVanilla<<< dG, dB>>>(y_gpu, A_gpu, IA_gpu, JA_gpu, x_gpu);
+                        // Transfer to device
+                        cudaMemcpy(A_gpu, A_cpu, NNZ*sizeof(float), cudaMemcpyHostToDevice);
+                        cudaMemcpy(IA_gpu, IA_cpu, (N+1)*sizeof(int), cudaMemcpyHostToDevice);
+                        cudaMemcpy(JA_gpu, JA_cpu, NNZ*sizeof(int), cudaMemcpyHostToDevice);
+                        cudaMemcpy(x_gpu, x_cpu, N*sizeof(float), cudaMemcpyHostToDevice);
+                        
+                        // CUDA kernel parameters
+                        //int sMem = (1 << 15);
+                        int dB, dG;
+                        if (N < 1024)
+                        {
+                                dB = N;
+                                dG = 1;
+                        }
+                        else
+                        {
+                                dB = BLOCK_SIZE;
+                                dG = N / 1024;
+                        }
+                        
+                        // Do CPU timing
+                        //gettimeofday(&t1, NULL);
+                        //cpuSpMV(y_cpu, A_cpu, IA_cpu, JA_cpu, N, x_cpu);
+                        //gettimeofday(&t2, NULL);
+                        //elapsed += (t2.tv_sec-t1.tv_sec)*1000.0 + (t2.tv_usec-t1.tv_usec)/1000.0; // in ms
+
+                        // Start cudaEvent timing
+                        cudaEventRecord(start);
+                        
+                        // CUDA Vanilla SpMV Kernel
+                        //spmvVanilla<<<dG, dB>>>(y_gpu, A_gpu, IA_gpu, JA_gpu, x_gpu);
+
+                        // CUDA Chocolate SpMV Kernel
+                        spmvChocolate<<<dG, dB>>>(y_gpu, A_gpu, IA_gpu, JA_gpu, x_gpu);
+                       
+                        // Stop cudaEvent timing
+                        cudaEventRecord(stop);
+                        cudaEventSynchronize(stop);
+
+                        // Check to make sure that cuda kernel was successful
+                        cudaError_t err = cudaGetLastError();
+                        if (err != cudaSuccess)
+                                printf("Error: %s\n", cudaGetErrorString(err));
+
+                        // Record timing result
+                        milliseconds = 0;
+                        cudaEventElapsedTime(&milliseconds, start, stop);
+                        elapsed += milliseconds;
+
+                        // Transfer result back to host
+                        cudaMemcpy(y_cpu, y_gpu, N*sizeof(float), cudaMemcpyDeviceToHost);
+
+                        // Test correctness of CUDA kernel vs "golden" cpu spmv function
+                        cpuSpMV(y_correct, A_cpu, IA_cpu, JA_cpu, N, x_cpu);
+                        if (!areEqualRMSE(y_correct, y_cpu, N))
+                                printf("Not correct result for a (%ix%i)*(%ix1) spmv multiplication\n", N, N, N);
+
+                        // Free memory
+                        free(A_cpu);
+                        free(IA_cpu);
+                        free(JA_cpu);
+                        free(x_cpu);
+                        free(y_cpu);
+                        free(y_correct);
+                        cudaFree(A_gpu);
+                        cudaFree(IA_gpu);
+                        cudaFree(JA_gpu);
+                        cudaFree(x_gpu);
+                        cudaFree(y_gpu);
+                }
+                t_arr[idx++] = (float)elapsed/NUM_ITERS;
+        }
+
+        printf("Results averaged over %i iterations with time in ms:\n", NUM_ITERS);
+        printf("N = "); printArray(N_arr, L);
+        printf("t = "); printArray(t_arr, L);
         
-        cudaEventRecord(stop);
-        cudaEventSynchronize(stop);
-
-        // Print result
-        float milliseconds = 0;
-        cudaEventElapsedTime(&milliseconds, start, stop);
-        printf("Elapsed time (ms) = %f\n", milliseconds);
-
-        // Transfer to host
-        cudaMemcpy(y_cpu, y_gpu, N*sizeof(float), cudaMemcpyDeviceToHost);
-        //printf("Output vector y: "); printArray(y_cpu, N);
-
-        // Verify correctness of CUDA kernel
-        y_correct = (float *)malloc(sizeof(float)*N);
-        cpuSpMV(y_correct, A_cpu, IA_cpu, JA_cpu, N, x_cpu);
-        //printf("Correct output vector y_correct: "); printArray(y_correct, N);
-
-        // Free memory
-        free(A_cpu);
-        free(IA_cpu);
-        free(JA_cpu);
-        free(x_cpu);
-        free(y_cpu);
-        free(y_correct);
-        cudaFree(A_gpu);
-        cudaFree(IA_gpu);
-        cudaFree(JA_gpu);
-        cudaFree(x_gpu);
-        cudaFree(y_gpu);
+        free(t_arr);
+        free(N_arr);
 
         cudaDeviceReset();
+
+        gettimeofday(&t2_main, NULL);
+        //elapsed += (t2.tv_sec-t1.tv_sec)*1000.0 + (t2.tv_usec-t1.tv_usec)/1000.0; // in ms
+        elapsedMain = (t2_main.tv_sec-t1_main.tv_sec) + (t2_main.tv_usec-t1_main.tv_usec)/1000000.0;
+        printf("Total time to execute main() : %g seconds\n", elapsedMain);
 	return 0;
 }
